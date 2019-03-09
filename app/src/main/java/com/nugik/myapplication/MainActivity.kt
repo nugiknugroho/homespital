752fb03fea4ce236128ca808d1138b00ecf67a31
nugiknugroho <nugiknugroho9897@gmail.com>
2019-03-09 09:24:56 +0700

    LIMA

---
+++ app/src/main/java/com/nugik/myapplication/MainActivity.kt
@@ -0,0 +1,30 @@
+package com.nugik.myapplication
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val button = findViewById(R.id.btn_register) as Button
+        button.setOnClickListener {
+            val intent = Intent(this, Main2Activity::class.java)
+            startActivity(intent)
+        }
+
+        val button1 = findViewById(R.id.btn_login) as Button
+        button1.setOnClickListener {
+            val intent = Intent(this, RecyclerView::class.java)
+            startActivity(intent)
+        }
+     }
+
+}
---
+++ app/src/main/java/com/nugik/myapplication/RecyclerView.kt
@@ -0,0 +1,12 @@
+package com.nugik.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RecyclerView : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recycler_view)
+    }
+}
