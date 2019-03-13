db54df350d596e3c44390ffc3bb9e0b99855d35f
nugiknugroho <nugiknugroho9897@gmail.com>
2019-03-13 18:35:21 +0700

    terupdate

--- app/src/main/java/com/nugik/myapplication/Main2Activity.kt
+++ app/src/main/java/com/nugik/myapplication/Main2Activity.kt
@@ -1,12 +1,20 @@
 package com.nugik.myapplication
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class Main2Activity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
+
+        val button = findViewById(R.id.btn_daftar) as Button
+        button.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
