0f2a8ae9b5965701864cae4ed46433708f39da00
fahrulakbar <fahrulakbar16@gmail.com>
2019-03-20 04:10:19 +0700

    commit

---
+++ app/src/main/java/com/nugik/myapplication/ApiEndPoint.kt
@@ -0,0 +1,16 @@
+package com.nugik.myapplication
+
+class ApiEndPoint {
+
+    companion object {
+
+        private val SERVER = "http://fahrulakbar.000webhostapp.com/homepital/"
+        val CREATE = SERVER+"register.php"
+        val READ = SERVER+"read.php"
+        val DELETE = SERVER+"delete.php"
+        val UPDATE = SERVER+"update.php"
+        val READ_LOGIN = SERVER+"read_login.php"
+
+    }
+
+}
