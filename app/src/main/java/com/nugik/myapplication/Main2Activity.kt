64df12adc7db9242cfe460e87ceddb5c478dde0f
fahrulakbar <fahrulakbar16@gmail.com>
2019-03-20 03:58:52 +0700

    commit

--- app/src/main/java/com/nugik/myapplication/Main2Activity.kt
+++ app/src/main/java/com/nugik/myapplication/Main2Activity.kt
@@ -1,55 +1,98 @@
 package com.nugik.myapplication
 
+import android.app.ProgressDialog
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.widget.Button
-import android.widget.EditText
-import android.app.DatePickerDialog
-import android.widget.DatePicker
-import java.text.SimpleDateFormat
-import android.view.View
-import android.support.annotation.RequiresApi
 import android.widget.Toast
-import android.os.Build
-import android.widget.TextView
+import com.androidnetworking.AndroidNetworking
+import com.androidnetworking.common.Priority
+import com.androidnetworking.error.ANError
+import com.androidnetworking.interfaces.JSONObjectRequestListener
 import kotlinx.android.synthetic.main.activity_main2.*
-import java.util.*
+import org.json.JSONObject
 
 class Main2Activity : AppCompatActivity() {
+    lateinit var i:Intent
+    private var jk="1"
 
-    var edit_Date: EditText? = null
-    var cal = Calendar.getInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main2)
 
+
         val button = findViewById(R.id.btn_daftar) as Button
         button.setOnClickListener {
-            val intent = Intent(this, MainActivity::class.java)
-            startActivity(intent)
+            register()
         }
 
 
+
+
+
+           gender.setOnCheckedChangeListener{ radioGroup, i ->
+
+            when(i){
+
+                R.id.laki->{
+                    jk = "1"
+                }
+
+                R.id.cewek->{
+                    jk = "2"
+                }
+
+            }
+
+        }
+
+
+
+        }
+
+
+    private fun register(){
+
+        val loading = ProgressDialog(this)
+        loading.setMessage("Menambahkan data...")
+        loading.show()
+
+        AndroidNetworking.post(ApiEndPoint.CREATE)
+                .addBodyParameter("nama",nama.text.toString())
+                .addBodyParameter("email",email.text.toString())
+                .addBodyParameter("gender",jk)
+                .addBodyParameter("alamat",alamat.text.toString())
+                .addBodyParameter("ttl",ttl.text.toString())
+                .addBodyParameter("username",username.text.toString())
+                .addBodyParameter("password",password.text.toString())
+                .setPriority(Priority.MEDIUM)
+                .build()
+                .getAsJSONObject(object : JSONObjectRequestListener {
+
+                    override fun onResponse(response: JSONObject?) {
+
+                        loading.dismiss()
+                        Toast.makeText(applicationContext,response?.getString("message"), Toast.LENGTH_SHORT).show()
+
+                        if(response?.getString("message")?.contains("successfully")!!){
+                            val intent = Intent(this@Main2Activity, MainActivity::class.java)
+                            startActivity(intent)
+                        }
+
+                    }
+
+                    override fun onError(anError: ANError?) {
+                        loading.dismiss()
+                        Log.d("ONERROR",anError?.errorDetail?.toString())
+                        Toast.makeText(applicationContext,"Connection Failure", Toast.LENGTH_SHORT).show()                    }
+
+
+                })
+
     }
-
-    @RequiresApi(Build.VERSION_CODES.N)
-    fun ClickDate(view: View) {
-        val c = Calendar.getInstance()
-        val year = c.get(Calendar.YEAR)
-        val month = c.get(Calendar.MONTH)
-        val day = c.get(Calendar.DAY_OF_MONTH)
-
-        val dpd = DatePickerDialog(this, DatePickerDialog.OnDateSetListener {
-            view, year, monthOfYear, dayOfMonth ->
-            // Display Selected date in Toast
-            tv_date.setText(""+dayOfMonth+"/"+(monthOfYear+1)+"/"+year)
-            Toast.makeText(this, """$dayOfMonth - ${monthOfYear + 1} - $year""", Toast.LENGTH_LONG).show()
-        }, year, month, day)
-
-        dpd.show()
-    }
-
 }
+
 
