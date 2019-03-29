688451d8c6250bb9eab929fea9213e197ccc67a4
nugiknugroho <nugiknugroho9897@gmail.com>
2019-03-29 21:25:14 +0700

    yeah2okokok

---
+++ app/src/main/java/com/nugik/myapplication/DetailActivityMenu/Ambulance/AmbulanceMapsActivity.kt
@@ -0,0 +1,13 @@
+package com.nugik.myapplication.DetailActivityMenu.Ambulance
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.nugik.myapplication.R
+
+class AmbulanceMapsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ambulance_maps)
+    }
+}
